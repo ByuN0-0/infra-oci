55184ecadd65155070057febb997e2cdfea38783
ByuN0-0 <devbyeon@gmail.com>
2026-05-07 23:07:00 +0900

    fix(network): Cloudflare ingress 제한 복원
    Cloudflare Proxy 재사용을 위해 API LB 공개 ingress 제한 복원
    DNS Only 전환용 0.0.0.0/0 허용 설정 제거
    LB NSG와 public subnet Security List가 Cloudflare CIDR만 허용하도록 반영

--- my_hub_api.tf
+++ my_hub_api.tf
@@ -1,27 +1,25 @@
 locals {
   ocir_endpoint        = "${var.region}.ocir.io"
   my_hub_api_image_url = "${local.ocir_endpoint}/${var.namespace}/${var.my_hub_api_repository_name}:${var.my_hub_api_image_tag}"
-  public_http_ingress_cidrs = toset(["0.0.0.0/0"])
+  public_http_ingress_cidrs = local.cloudflare_ipv4_cidrs
 
-  # Cloudflare proxy mode used these CIDRs instead of public ingress.
-  # Keep the list here so switching back is just a local reference change.
-  # cloudflare_ipv4_cidrs = toset([
-  #   "103.21.244.0/22",
-  #   "103.22.200.0/22",
-  #   "103.31.4.0/22",
-  #   "104.16.0.0/13",
-  #   "104.24.0.0/14",
-  #   "108.162.192.0/18",
-  #   "131.0.72.0/22",
-  #   "141.101.64.0/18",
-  #   "162.158.0.0/15",
-  #   "172.64.0.0/13",
-  #   "173.245.48.0/20",
-  #   "188.114.96.0/20",
-  #   "190.93.240.0/20",
-  #   "197.234.240.0/22",
-  #   "198.41.128.0/17",
-  # ])
+  cloudflare_ipv4_cidrs = toset([
+    "103.21.244.0/22",
+    "103.22.200.0/22",
+    "103.31.4.0/22",
+    "104.16.0.0/13",
+    "104.24.0.0/14",
+    "108.162.192.0/18",
+    "131.0.72.0/22",
+    "141.101.64.0/18",
+    "162.158.0.0/15",
+    "172.64.0.0/13",
+    "173.245.48.0/20",
+    "188.114.96.0/20",
+    "190.93.240.0/20",
+    "197.234.240.0/22",
+    "198.41.128.0/17",
+  ])
 
   my_hub_api_environment_variables = merge(
     {
